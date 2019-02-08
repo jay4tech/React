5cb1353aaa759448ca805ae21aa725a09bd57a55
Jay <jay.p@mapmyindia.com>
2019-02-08 18:38:10 +0530

    Chapter-12

--- myapp/src/Employee.js
+++ myapp/src/Employee.js
@@ -1,21 +1,52 @@
 import React from 'react';
 
-const Employee = ({employees}) => {
-    const employeeList = employees.map(employee => {
-        return (
-            <div className="employee" key={employee.id}>
-                <div>Name: {employee.name}</div>
-                <div>Age: {employee.age}</div>
-                <div>Salary:{employee.salary}</div>
-                <hr />
-            </div>
-        )
-    })
+const Employee = ({ employees }) => {
+    // const employeeList = employees.map(employee => {
+    //    if(employee.age>20){
+    //    return (
+    //         <div className="employee" key={employee.id}>
+    //             <div>Name: {employee.name}</div>
+    //             <div>Age: {employee.age}</div>
+    //             <div>Salary:{employee.salary}</div>
+    //             <hr />
+    //         </div>
+    //     )
+    //    }else{
+    //        return null;
+    //    }
+    // })
+
+    // const employeeList = employees.map(employee => {
+
+    //     return employee.age  < 30 ? (<div className="employee" key={employee.id}>
+    //         <div>Name: {employee.name}</div>
+    //         <div>Age: {employee.age}</div>
+    //         <div>Salary:{employee.salary}</div>
+    //         <hr />
+    //     </div>) : null;
+    // })
+    // return (
+    //     <div className="employee-list" >
+    //         {employeeList}
+    //     </div >
+
+    // )
+
     return (
         <div className="employee-list" >
-            {employeeList}
-        </div >
+            {
+                employees.map(employee => {
+                    return employee.age > 20 ? (<div className="employee" key={employee.id}>
+                        <div>Name: {employee.name}</div>
+                        <div>Age: {employee.age}</div>
+                        <div>Salary:{employee.salary}</div>
+                        <hr />
+                    </div>
+                    ) : null;
 
+                })
+            }
+        </div>
     )
 }
 export default Employee;
