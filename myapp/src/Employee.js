1ff86658fc66432964bd1694dfc158479705101a
Jay <jay.p@mapmyindia.com>
2019-02-08 18:14:00 +0530

    Chapter-11

--- myapp/src/Employee.js
+++ myapp/src/Employee.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Employee extends Component {
-    
-    render(){
-        const {employees}=this.props;
-        const employeeList= employees.map(employee=>{
-            return(
+const Employee = ({employees}) => {
+    const employeeList = employees.map(employee => {
+        return (
             <div className="employee" key={employee.id}>
                 <div>Name: {employee.name}</div>
                 <div>Age: {employee.age}</div>
                 <div>Salary:{employee.salary}</div>
-                <hr/>
+                <hr />
             </div>
-            )
-        })
-        return(
-            <div className="employee-list">
-            {employeeList}
-            </div>
-           
         )
-    }
+    })
+    return (
+        <div className="employee-list" >
+            {employeeList}
+        </div >
+
+    )
 }
 export default Employee;
