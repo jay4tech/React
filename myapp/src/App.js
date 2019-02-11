341090f1a8a68327e0bd1fafbb9b0c3e0ad27071
Jay <jay.p@mapmyindia.com>
2019-02-11 10:46:54 +0530

    Chapter-14

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
-import Employee  from './Employee';
-import AddEmployee  from './AddEmployee';
+import Employee from './Employee';
+import AddEmployee from './AddEmployee';
 class App extends Component {
-  state={
-    employees:[
-      { name:'Jay', age:25 ,salary:2000,id:1},
-      { name:'Jaya', age:50, salary:3000,id:2},
-      { name:'Raj', age:20, salary:4000,id:3}
-     ]
+  state = {
+    employees: [
+      { name: 'Jay', age: 25, salary: 2000, id: 1 },
+      { name: 'Jaya', age: 50, salary: 3000, id: 2 },
+      { name: 'Raj', age: 20, salary: 4000, id: 3 }
+    ]
   }
-  addEmployee=(employee)=>{
-        
+  addEmployee = (employee) => {
+    console.log(employee)
+    employee.id = Math.random();
+    let employees = [...this.state.employees, employee]
+    this.setState({
+      employees: employees
+    })
   }
   render() {
     return (
       <div className="App">
         <h1>My First React App</h1>
         <p>Welcome students</p>
-        <Employee employees={this.state.employees}/>
-        <AddEmployee addEmployee={this.addEmployee}/>
+        <Employee employees={this.state.employees} />
+        <AddEmployee addEmployee={this.addEmployee} />
       </div>
     );
   }
